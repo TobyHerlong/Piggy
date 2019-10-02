16c55742499ee0ea717495a917e62d2429da417b
Tobias Herlong <herlongt@gilmour.org>
2019-10-02 09:23:46 -0400

    new gyro turn commands

--- teacher.py
+++ teacher.py
@@ -79,6 +79,36 @@ class PiggyParent(gopigo3.GoPiGo3):
         self.offset_motor_encoder(self.MOTOR_RIGHT, self.get_motor_encoder(self.MOTOR_RIGHT))
         self.set_motor_position(self.MOTOR_LEFT + self.MOTOR_RIGHT, deg)
 
+    def turn_by_deg(self, deg):
+        # higher - order 
+        
+        # get our current location
+        current = self.get_heading()
+
+        # calculate delta
+        goal = current + deg
+        if goal > 360:
+            goal -= 360
+        elif goal < 360:
+            goal += 360
+
+        # call turn to deg on the delta
+        self.turn_to_deg(goal)
+        
+
+    def turn_to_deg(self, deg):
+        # lower - order
+
+        # extra credit if you makeit turn left 
+
+        # while - loop keep turing until my gyro saysi'm there
+        while abs(deg - self.get_heading()) < 5:
+            self.right(primary=60, counter=-60)
+        self.stop()
+        print("I think i've turned correctly")
+
+
+
     def fwd(self, left=50, right=50):
         """Blindly charges your robot forward at default power which needs to be configured in child class"""
         if self.LEFT_DEFAULT and left == 50:
