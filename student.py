28bece200a1212ee596040ab35919ad94c788197
Tobias Herlong <herlongt@gilmour.org>
2019-11-06 10:50:36 -0500

    resets to midpoint

--- student.py
+++ student.py
@@ -97,6 +97,7 @@ class Piggy(PiggyParent):
         print("-----------! NAVIGATION ACTIVATED !------------\n")
         print("Wait a second. \nI can't navigate the maze at all. Please give my programmer a zero.")
         while True:    
+            self.servo(self.MIDPOINT)
             while self.read_distance() > 150:
                 self.fwd()
                 time.sleep(.01)
