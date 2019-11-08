111a08374772a37bedbafb5228e17afeab87094c
Tobias Herlong <herlongt@gilmour.org>
2019-11-08 08:42:19 -0500

    speed up scan

--- student.py
+++ student.py
@@ -67,7 +67,7 @@ class Piggy(PiggyParent):
     
     def scan(self):
         """Sweep the servo and populate the scan_data dictionary"""
-        for angle in range(self.MIDPOINT-350, self.MIDPOINT+350, 25):
+        for angle in range(self.MIDPOINT-350, self.MIDPOINT+350, 100):
             self.servo(angle)
             self.scan_data[angle] = self.read_distance()
 
@@ -126,6 +126,7 @@ class Piggy(PiggyParent):
             # self.turn_by_deg(46)
 
     def checkways():
+        #smart attempt
         self.servo(1000)
         time.sleep(.3)
         r = self.read_distance()
